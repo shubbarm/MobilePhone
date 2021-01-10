014d1db09d7b6b7be4f907add8373c54f5c0fd1a
Mustafa Shubbar <codingbox@outlook.com>
2021-01-09 20:39:20 -0500

    init

---
+++ src/mobilephone/Main.java
@@ -0,0 +1,16 @@
+package mobilephone;
+
+/**
+ *
+ * @author Mustafa Shubbar <codingbox@outlook.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
